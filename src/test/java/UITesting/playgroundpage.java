04c3a5ab3fe2af7819cba73efe34a8218ce66585
FaizaSafeer <faiza.safeer456@gmail.com>
2023-06-27 17:01:12 +1000

    done

--- src/test/java/UITesting/playgroundpage.java
+++ src/test/java/UITesting/playgroundpage.java
@@ -1,10 +1,13 @@
 package UITesting;
 
+import dev.failsafe.internal.util.Assert;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class playgroundpage {
 
@@ -26,6 +29,13 @@ driver.findElement(By.id("submit")).click();
            WebDriver driver = new ChromeDriver(options);
            driver.get("https://d18u5zoaatmpxx.cloudfront.net/#/");
            driver.manage().window().maximize();
+
+
+           String text=driver.findElement(By.xpath("//h1[@class='display-1 mb-3']")).getText();
+           System.out.println(text);
+
+           assertEquals(text,"Web Playground");
+
 
        }
 
